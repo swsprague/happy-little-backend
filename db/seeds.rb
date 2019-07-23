8a78ecab507851727dc0732af277ff039aac9d29
sethwsprague <sethwsprague@gmail.com>
2019-07-23 12:19:27 -0400

    Add video csv & seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create([{ youtube_id: 'kdlHV6ceI_g', episode_number: 701, name: 'Winter Cabin', description: 'In the midst of a cold, wintry forest, Bob Ross gently places a magical little wooden shanty upon a bed of soft snow.', air_date: '1985-10-02', season_num: 7 }])
+require '../../data/videos.csv'
+
+# Video.create([{ youtube_id: 'kdlHV6ceI_g', episode_number: 701, name: 'Winter Cabin', description: 'In the midst of a cold, wintry forest, Bob Ross gently places a magical little wooden shanty upon a bed of soft snow.', air_date: '1985-10-02', season_num: 7 }])
+
+require 'csv'
+
+Video.transaction do
+  CSV.foreach(Rails.root + 'data/videos.csv',
+              headers: true,
+              header_converters: ->(h) { h.downcase.to_sym }) do |video_row|
+    video = video_row.to_hash
+    Video.create video unless Video.exists? video
+  end
+end
