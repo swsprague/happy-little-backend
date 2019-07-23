87618c75d8ce1bdb0e98b5e8c7d4300315a3a658
sethwsprague <sethwsprague@gmail.com>
2019-07-23 13:22:13 -0400

    Moved data to root and edit csv filename

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 require 'csv'
 
 Video.transaction do
-  CSV.foreach('app/data/videos.csv',
+  CSV.foreach(Rails.root + 'data/videos.csv',
               headers: true,
               header_converters: ->(h) { h.downcase.to_sym }) do |video_row|
     video = video_row.to_hash
